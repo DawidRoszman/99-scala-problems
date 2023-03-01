7265bfe224582c88764591e33a012bf614433e51
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2023-03-01 07:53:31 +0100

    added ex as comments

---
+++ src/main/scala/Learning1.scala
@@ -0,0 +1,13 @@
+//P01 (*) Find the last element of a list.
+
+//PO2 (*) Find the last but one element of a list.
+
+//P03 (*) Find the Kth element of a list.
+
+//P06 (*) Find out whether a list is a palindrome.
+
+//P32 (*) Determine the greatest common divisor of two positive integer numbers.
+
+//P39 (*) A list of prime numbers.
+
+//P49 (**) Gray code.
