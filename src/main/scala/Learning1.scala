fc6857ff428617f32b01620154a5940839dec896
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2023-03-02 19:47:21 +0100

    finished day 1 learning
    Co-authored-by: Mateusz Kurowski <mateusz-kurowski@users.noreply.github.com>

--- src/main/scala/Learning1.scala
+++ src/main/scala/Learning1.scala
@@ -1,13 +1,108 @@
-//P01 (*) Find the last element of a list.
+val arr = List(2, 23, -23, 12, 55, 7, -8, 5, 9, -54)
+val arr2 = List(1, 2, 2, 1)
 
+@main
+def Main(): Unit = {
+  println(listPalindrome(arr2))
+}
+
+//P01 (*) Find the last element of a list.
+// head (mozna uzyc innego slowa) - pierwszy element
+// tail (mozna uzyc innego slowa) - reszta listy
+// head :: tail - lista złożona z heada i taila
+// Nil - pusta lista
+// lastElement[A] - funkcja, która przyjmuje listę elementów typu A i zwraca element typu A
+//Krok 1: head = 2, tail = [23, -23, 12, 55, 7, -8, 5, 9, -54]
+//_ :: tail
+// head = -54 tail = []
+//Krok 2: head = 23 tail = [-23, 12, 55, 7, -8, 5, 9, -54]
+//Krok 3: head = -23 tail = [12, 55, 7, -8, 5, 9, -54]
+//Krok 4: head = 12 tail = [55, 7, -8, 5, 9, -54]
+//Krok 5: head = 55 tail = [7, -8, 5, 9, -54]
+//Krok 6: head = 7 tail = [-8, 5, 9, -54]
+//Krok 7: head = -8 tail = [5, 9, -54]
+//Krok 8: head = 5 tail = [9, -54]
+//Krok 9: head = 9 tail = [-54]
+//Krok 10: head = -54 tail = []
+// head :: head2 :: tail - lista złożona z heada i head2 i taila
+//A - any type
+// Nil == []
+def lastElement[A](list: List[A]): A = {
+  list match {
+    case head :: Nil => head
+    case _ :: tail => lastElement(tail)
+    case _ => throw new NoSuchElementException
+  }
+}
 //PO2 (*) Find the last but one element of a list.
+
+def lastElement2[A](list: List[A]): A = {
+  list match {
+    case head :: _ :: Nil => head
+    case _ :: tail => lastElement2(tail)
+    case _ => throw new NoSuchElementException
+  }
+}
 
 //P03 (*) Find the Kth element of a list.
 
-//P06 (*) Find out whether a list is a palindrome.
+def kthElement[A](list: List[A], n: Int): A = {
+  (list, n) match {
+    case (head :: _, 0) => head
+    case (_ :: tail, n) => kthElement(tail, n - 1)
+    case (Nil, _) => throw new NoSuchElementException
+  }
+}
 
+//P05 (*) Reverse a list.
+def reverseTailRecursive[A](ls: List[A]): List[A] = {
+  def reverseR(result: List[A], curList: List[A]): List[A] =
+    curList match {
+      case Nil => result
+      case h :: tail => reverseR(h :: result, tail)
+    }
+  reverseR(Nil, ls)
+}
+// h :: result - lista złożona z heada i resulta (head :: result)
+// Krok 1: result = [], curList = [2, 23, -23, 12, 55, 7, -8, 5, 9, -54]
+// head = 2, tail = [23, -23, 12, 55, 7, -8, 5, 9, -54]
+//Krok 2: result = [2], curList = [23, -23, 12, 55, 7, -8, 5, 9, -54]
+// head = 23, tail = [-23, 12, 55, 7, -8, 5, 9, -54]
+//Krok 3: result = [23, 2], curList = [-23, 12, 55, 7, -8, 5, 9, -54]
+//Krok 4: result = [-23, 23, 2], curList = [12, 55, 7, -8, 5, 9, -54]
+//Krok 5: result = [12, -23, 23, 2], curList = [55, 7, -8, 5, 9, -54]
+//Krok 6: result = [55, 12, -23, 23, 2], curList = [7, -8, 5, 9, -54]
+//Krok 7: result = [7, 55, 12, -23, 23, 2], curList = [-8, 5, 9, -54]
+//Krok 8: result = [-8, 7, 55, 12, -23, 23, 2], curList = [5, 9, -54]
+//Krok 9: result = [5, -8, 7, 55, 12, -23, 23, 2], curList = [9, -54]
+//Krok 10: result = [9, 5, -8, 7, 55, 12, -23, 23, 2], curList = [-54]
+//Krok 11: result = [-54, 9, 5, -8, 7, 55, 12, -23, 23, 2], curList = []
+//Zwracamy result
+
+//P06 (*) Find out whether a list is a palindrome.
+def listPalindrome[A](list: List[A]): Boolean = {
+  val reversedList = reverseTailRecursive(list)
+  if list == reversedList then true else false
+}
+// if(n == 9){ return 9}
+// else if(n == 2) { return 2}
+// else { return 0}
+// n match {
+//   case 9 => 9
+//   case 2 => 2
+//   case _ => 0
+// }
 //P32 (*) Determine the greatest common divisor of two positive integer numbers.
 
 //P39 (*) A list of prime numbers.
 
 //P49 (**) Gray code.
+
+def lepszaSilnia2(n: Int, akumulator: Int = 1): Int = {
+  @annotation.tailrec
+  def go(n: Int, akumulator: Int): Int = n match {
+    case 1 => akumulator
+    case _ => go(n - 1, akumulator * n)
+  }
+  go(n, akumulator)
+}
---
+++ src/main/scala/message.scala
@@ -0,0 +1,114 @@
+// typy argumentów są obowiązkowe, co zwraca funkcja niekoniecznie - scala sobie sama sprawdzi, ale powinniśmy ją dawać
+def inc1(n: Int) = {
+  n + 1 // :Int
+  println(n + 1) // () :Unit
+  // funkcja zwraca to co jest ostatnie
+}
+
+// powinniśmy jawnie pokazywać co funkcja zwraca
+def inc2(n: Int): Int = {
+  n + 2
+}
+
+// funkcję można również zapisać w taki sposób, jeśli mamy tylko jedno wyrażenie
+def inc2a(n: Int): Int = n + 2
+
+lazy val dziwne = {
+  val x = 123
+  x + 1 // :Int
+  println(x + 1) // () :Unit
+  x.toString // konwersja do stringa
+  x
+}
+
+// Nie trzeba pisać, że w1 to String, kompilator sam to ogarnie
+// val w1: String = if (dziwne > 100) "OK" else "nie OK" // wyrażenie warunkowe, a nie instrukcja warunkowa
+
+// Tutaj mamy typ zmiennej Any - ogólny typ (cokolwiek)
+// val w2 = if (dziwne > 100) "OK" else -1
+// Czyli mamy to samo co to:
+// val w2: Any = if (dziwne > 100) "OK" else -1
+
+// Powstaje problem przepełnienia stostu, więc możemy ją ulepszyć
+def silnia(n: Int): Int = {
+  if (n <= 0) 1
+  else n * silnia(n - 1)
+  // ten problem się buduje przez mnożenie w tym działaniu, bo "odkładamy je na później"
+}
+
+def silnia2(n: Int): Int = {
+  n match {
+    case 0 => 1
+    case _ => n * silnia2(n - 1)
+  }
+}
+def fibomatch(n: Int): Int =
+  n match {
+    case 0 | 1 => 1
+    case _ => fibomatch(n - 1) + fibomatch(n - 2)
+  }
+
+def fibTailRec(n: Int): Int = {
+  @annotation.tailrec // adnotacja, która sprawdza czy funkcja jest ogonowa
+  def go(n: Int, a: Int, b: Int): Int = n match
+    case 0 => a
+    case 1 => b
+    case _ => go(n - 1, b, a + b)
+  go(n, 0, 1)
+}
+
+@main
+def mainSilnia: Unit = {
+  val wynik = fibomatch(5)
+  println(wynik)
+}
+
+def funDomyslna(n: Int = 1): Unit = println("#" * n)
+
+// Żeby to zoptymalizować do funkcji będziemy podawać zarówno liczbę jak i to przez ile chcemy pomnożyć
+// Rekurencję można wykonywać ogonowo, czyli dochodzimy do końca i zwracamy wynik, a nie że musimy rozwijać ją z powrotem od końca
+
+// def lepszaSilnia(n: Int, akumulator: Int = 1): Int = {
+//   n match
+//     case 1 => akumulator
+//     case _ => lepszaSilnia(n - 1, akumulator * n)
+//   }
+// }
+
+def lepszaSilnia2(n: Int, akumulator: Int = 1): Int = {
+  @annotation.tailrec
+  def go(n: Int, akumulator: Int): Int = n match {
+    case 1 => akumulator
+    case _ => go(n - 1, akumulator * n)
+  }
+  go(n, akumulator)
+}
+
+@main def mainSilniaTail: Unit = {
+  val wynik = lepszaSilnia2(5)
+  println(wynik)
+}
+// ??? - wyrażenie odpowiedniego typu, które oznacza, że to jest coś co dopiero będzie zdefiniowane
+
+def fibo(n: Int): Int = {
+  if (n == 0 || n == 1) 1
+  else fibo(n - 1) + fibo(n - 2)
+}
+
+@main def mainProg: Unit = {
+  // val wynik = inc1(123)
+  // println(wynik)
+  // println(dziwne)
+
+  // val wynik2 = silnia(100) // wyszliśmy poza zakres Int, więc wynik = 0, blisko granicy, wyniki mogą wychodzić różne
+  // val wynik2 = silnia(100000) // stackOverflowError
+  // val wynik2 = silnia(5)
+  // println(wynik2)
+
+  funDomyslna() // Domyślne - 1
+  funDomyslna(20)
+  funDomyslna(n = 40) // Można go też jawnie podać
+
+  val wynik3 = fibo(10)
+  println(wynik3)
+}
